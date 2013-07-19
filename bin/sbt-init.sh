9da914a29bfe071ffbfd38b50ce220996e7be85a
hhulkko <heikki.hulkko@iki.fi>
2013-07-19 10:48:42 +0200

    Tweak sbt template

--- bin/sbt-init.sh
+++ bin/sbt-init.sh
@@ -14,14 +14,30 @@ cat > build.sbt<<EOF
 // Example settings http://www.scala-sbt.org/release/docs/Examples/Quick-Configuration-Examples
 name := "My Project"
 
-version := "1.0"
+version := "0.1"
 
 organization := "hhulkko"
 
 scalaVersion := "2.10.1"
 
-//libraryDependencies += "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test"
-//libraryDependencies += "joda-time" % "joda-time" % "2.1"
+libraryDependencies ++= Seq(
+  "net.databinder" %% "unfiltered-filter" % "0.6.8",
+  "net.databinder" %% "unfiltered-jetty" % "0.6.8",
+  "net.databinder" %% "unfiltered-filter-uploads" % "0.6.8",
+  "org.json4s" %% "json4s-native" % "3.2.4",
+  "com.typesafe.slick" %% "slick" % "1.0.0",
+  "mysql" % "mysql-connector-java" % "5.1.22",
+  "ch.qos.logback" % "logback-core" % "1.0.12",
+  "ch.qos.logback" % "logback-classic" % "1.0.12",
+  "com.jolbox" % "bonecp" % "0.7.1.RELEASE",
+  "com.typesafe" % "config" % "1.0.0",
+  "joda-time" % "joda-time" % "2.2",
+  "org.joda" % "joda-convert" % "1.3.1",
+  "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.2",
+  "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.2",
+  "org.specs2" %% "specs2" % "1.14" % "test",
+  "org.apache.httpcomponents" % "httpclient" % "4.2.5" % "test"
+)
 
 // append several options to the list of options passed to the Java compiler
 javacOptions ++= Seq("-source", "1.6", "-target", "1.6")
