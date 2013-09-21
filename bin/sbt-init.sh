1913bf65eac01e9e824d0bc06c1d9bed3adf2d05
hhulkko <heikki.hulkko@iki.fi>
2013-09-21 13:33:51 +0300

    Update default sbt dependencies

--- bin/sbt-init.sh
+++ bin/sbt-init.sh
@@ -6,10 +6,6 @@ chmod 755 sbt
 mkdir -p src/main/scala
 mkdir -p src/test/scala
 
-cat > build.properties<<EOF
-sbt.version=0.12.+
-EOF
-
 cat > build.sbt<<EOF
 // Example settings http://www.scala-sbt.org/release/docs/Examples/Quick-Configuration-Examples
 name := "My Project"
@@ -18,29 +14,25 @@ version := "0.1"
 
 organization := "hhulkko"
 
-scalaVersion := "2.10.1"
+scalaVersion := "2.10.2"
 
 libraryDependencies ++= Seq(
-  "net.databinder" %% "unfiltered-filter" % "0.6.8",
-  "net.databinder" %% "unfiltered-jetty" % "0.6.8",
-  "net.databinder" %% "unfiltered-filter-uploads" % "0.6.8",
-  "org.json4s" %% "json4s-native" % "3.2.4",
-  "com.typesafe.slick" %% "slick" % "1.0.0",
-  "mysql" % "mysql-connector-java" % "5.1.22",
-  "ch.qos.logback" % "logback-core" % "1.0.12",
-  "ch.qos.logback" % "logback-classic" % "1.0.12",
-  "com.jolbox" % "bonecp" % "0.7.1.RELEASE",
+  "net.databinder" %% "unfiltered-filter" % "0.7.0",
+  "net.databinder" %% "unfiltered-jetty" % "0.7.0",
+  "net.databinder" %% "unfiltered-filter-uploads" % "0.7.0",
+  "ch.qos.logback" % "logback-core" % "1.0.13",
+  "ch.qos.logback" % "logback-classic" % "1.0.13",
   "com.typesafe" % "config" % "1.0.0",
-  "joda-time" % "joda-time" % "2.2",
-  "org.joda" % "joda-convert" % "1.3.1",
+  "joda-time" % "joda-time" % "2.3",
+  "org.joda" % "joda-convert" % "1.4",
   "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.2",
   "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.2",
-  "org.specs2" %% "specs2" % "1.14" % "test",
+  "org.specs2" %% "specs2" % "2.2" % "test",
   "org.apache.httpcomponents" % "httpclient" % "4.2.5" % "test"
 )
 
 // append several options to the list of options passed to the Java compiler
-javacOptions ++= Seq("-source", "1.6", "-target", "1.6")
+javacOptions ++= Seq("-source", "1.7", "-target", "1.7")
 
 // append -deprecation to the options passed to the Scala compiler
 scalacOptions += "-deprecation"
